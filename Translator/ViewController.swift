e4c23f001942cff85191952c403627d4ec0fed0e
SwiftyCody <soldolly@gmail.com>
2023-02-04 14:33:13 +0900

    First Commit
    Initialize project file
    - Swift, UIKit, no storyboard base
    - dependancy: Alamofire, SnapKit, Then

---
+++ Translator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Translator
+//
+//  Created by solgoon on 2023/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
